5b4b5070f070f7d0c8b8e4e4ad63e412b6f8445f
Alexander Bowers <7319abowers@gmail.com>
2020-09-28 00:03:09 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -41,8 +41,8 @@ end
 def over?(board)
   if draw?(board)
     return true
-  elsif won?(board) && !(full?(board))
-    return true
+  #elsif won?(board) && !(full?(board))
+    #return true
   elsif won?(board) && full?(board)
     return true
   else
